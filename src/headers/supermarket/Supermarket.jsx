4f0f3a2e328111df262ddbf605d4d1552d606952
kiaradzangian <kiaradzangian@gmail.com>
2024-07-30 14:21:51 +0330

    edite

--- src/headers/supermarket/Supermarket.jsx
+++ src/headers/supermarket/Supermarket.jsx
@@ -1,92 +1,17 @@
+import Supermarketheader from './Supermarketheader';
+import Supermarketcards from './Supermarketcards';
 export default function Supermarket() {
   return (
     <div>
-      <nav className='bg-white border-gray-200 dark:bg-gray-900'>
-        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <a
-            href='https://flowbite.com/'
-            className='flex items-center space-x-3 rtl:space-x-reverse'
-          >
-            <img
-              src='https://www.digikala.com/fresh/statics/img/png/fresh-header-logo.webp'
-              className='h-12'
-              alt='Flowbite Logo'
-            />
-            <img
-              src='https://www.digikala.com/fresh/statics/img/svg/Fresh-Text-Logo.svg'
-              className='h-8'
-              alt='Flowbite Logo'
-            />
-            <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'></span>
-          </a>
-          <button
-            data-collapse-toggle='navbar-default'
-            type='button'
-            className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-            aria-controls='navbar-default'
-            aria-expanded='false'
-          >
-            <span className='sr-only'>Open main menu</span>
-            <svg
-              className='w-5 h-5'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 17 14'
-            ></svg>
-          </button>
-          <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
-            <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  بازگشت
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  دیجی مارکت
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  لبنیات
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  خوراکی
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  اماده
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      <Supermarketheader />
+      <hr />
+      <img
+        className='w-full h-96'
+        src='https://dkstatics-public.digikala.com/digikala-adservice-banners/b7f87400ffe4a1e650bf37f74ab52af03817404a_1721829928.jpg?x-oss-process=image/quality,q_95/format,webp'
+        alt=''
+      />
+      <Supermarketcards />
+      <div></div>
     </div>
   );
 }
---
+++ src/headers/supermarket/Supermarketcards.jsx
@@ -0,0 +1,20 @@
+export default function Supermarketcards() {
+  return (
+    <div>
+      <div className='bg-green-500 p-8 rounded-2xl m-5'>
+        <div className='w-24'>
+          <img
+            src='https://www.digikala.com/fresh/statics/img/svg/specialCarousel/FeaturedPromos.svg'
+            alt=''
+          />
+          <img
+            className='w-24'
+            src='https://www.digikala.com/fresh/statics/img/png/specialCarousel/fresh-incredible-carousel-artwork.webp'
+            alt=''
+          />
+          <p className='gap-4 text-white border border-white p-1 rounded-lg'>مشاهده همه</p>
+        </div>
+      </div>
+    </div>
+  );
+}
