6bd2c5db73b23348c367605a3eaf0fac9971ab24
kiaradzangian <kiaradzangian@gmail.com>
2024-07-31 13:21:34 +0330

    edite

--- src/app/pages/product/index.jsx
+++ src/app/pages/product/index.jsx
@@ -7,7 +7,7 @@ const Product = () => {
       <br></br> <br></br> <br></br> <br></br> <br></br>
       <br></br> <br></br>
       <div className='flex justify-center bg-red-500 h-16 '>
-        <h1 className='p-4 font-sans hover:font-serif text-2xl text-center font-bold text-white'>
+        <h1 className='p-4 font-sans hover:font-mono   text-2xl text-center font-bold text-white'>
           پیشنهاد شگفت انگیز
         </h1>
       </div>
